3da9fe1a4695f52b159d5b27cdcde2b1e36cbcd9
tevolx <tevolx0@gmail.com>
2024-12-23 18:26:05 +0900

    add chapter7

--- chapter1/src/main/java/hello/itemservice/config/QuerydslConfig.java
+++ chapter1/src/main/java/hello/itemservice/config/QuerydslConfig.java
@@ -1,19 +1,19 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
-import hello.itemservice.repository.jpa.JpaItemRepositoryV2;
-import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV3;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class SpringDataJpaConfig {
+public class QuerydslConfig {
 
-    private final SpringDataJpaItemRepository itemRepository;
+    private final EntityManager em;
 
     @Bean
     public ItemService itemService() {
@@ -22,7 +22,6 @@ public class SpringDataJpaConfig {
 
     @Bean
     public ItemRepository itemRepository() {
-        return new JpaItemRepositoryV2(itemRepository);
+        return new JpaItemRepositoryV3(em);
     }
-
 }
