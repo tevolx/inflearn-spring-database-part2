7896a8e5a4558be79e54e34dd82d846c4a0d01e9
tevolx <tevolx0@gmail.com>
2024-12-29 17:10:31 +0900

    feat: add chapter11

---
+++ chapter9-11/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,149 @@
+package hello.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    LogRepository logRepository;
+
+    /**
+     * MemberService @Transactional: OFF
+     * MemberRepository @Transactional: ON
+     * LogRepository @Transactional: ON
+     */
+    @Test
+    void outerTxOff_success() {
+        //given
+        String username = "outerTxOff_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * MemberService @Transactional: OFF
+     * MemberRepository @Transactional: ON
+     * LogRepository @Transactional: ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when
+        assertThatThrownBy(() ->  memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * MemberService @Transactional: ON
+     * MemberRepository @Transactional: OFF
+     * LogRepository @Transactional: OFF
+     */
+    @Test
+    void singleTx() {
+        //given
+        String username = "singleTx";
+
+        //when
+        memberService.joinV1(username);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * MemberService @Transactional: ON
+     * MemberRepository @Transactional: ON
+     * LogRepository @Transactional: ON
+     */
+    @Test
+    void outerTxOn_success() {
+        //given
+        String username = "outerTxOn_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * MemberService @Transactional: ON
+     * MemberRepository @Transactional: ON
+     * LogRepository @Transactional: ON exception
+     */
+    @Test
+    void outerTxOn_fail() {
+        //given
+        String username = "로그예외_outerTxOn_fail";
+
+        //when
+        assertThatThrownBy(() ->  memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+
+        //then
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * MemberService @Transactional: ON
+     * MemberRepository @Transactional: ON
+     * LogRepository @Transactional: ON exception
+     */
+    @Test
+    void recoverException_fail() {
+        //given
+        String username = "로그예외_recoverException_fail";
+
+        //when
+        assertThatThrownBy(() ->  memberService.joinV2(username)).isInstanceOf(UnexpectedRollbackException.class);
+
+        //then
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * MemberService @Transactional: ON
+     * MemberRepository @Transactional: ON
+     * LogRepository @Transactional: ON (REQUIRES_NEW) exception
+     */
+    @Test
+    void recoverException_success() {
+        //given
+        String username = "로그예외_recoverException_success";
+
+        //when
+        memberService.joinV2(username);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+}
